23f79c5d40624f632768c0bf846afc1e6e347bf7
Kauany Calais <kauanycalais10@gmail.com>
2024-09-19 14:27:53 -0300

    Alteração do arquivo Readme

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,23 @@
+﻿
+		string[] opcoes ={ "papel", "pedra", "tesoura" };
+		Random random = new Random();
+		Console.WriteLine("Bem-Vindo ao jogo Pedra, Papel, Tesoura!");
+		while (true)
+		{
+			Console.WriteLine("Escolha entre pedra, papel ou tesoura (ou digite sair para terminar o jogo): ");
+			string jogador = Console.ReadLine().ToLower();
+			if (jogador == "sair")
+			{
+				break;
+			}
+
+			string computador = opcoes[random.Next(opcoes.Length)];
+			Console.WriteLine($"O computador escolheu: {computador}");
+			if (jogador == computador)
+				Console.WriteLine(" Empate! ");
+			else if (jogador == "pedra" && computador == "tesoura" || jogador == "papel" && computador == "pedra" || jogador == "tesoura" && computador == "papel")
+				Console.WriteLine(" Você venceu! ");
+			else
+				Console.WriteLine(" Computador venceu! ");
+			Console.WriteLine();
+		}
